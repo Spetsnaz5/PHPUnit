b2baff4a09df2475de33b307a2acc7614f469dc2
liao yi an <4a0g0134@gmail.com>
2025-12-02 09:16:11 +0800

    unit test

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -7,5 +7,24 @@ final class ExceptionTest extends TestCase
     public function testException(): void
     {
         $this->expectException(InvalidArgumentException::class);
+
+        throw new InvalidArgumentException('Invalid amount');
+    }
+
+    public function testExceptionCode()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionCode(404);
+
+        throw new \Exception('Order not found', 404);
+    }
+
+    public function testExceptionMessage(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->expectExceptionMessage('Invalid amount');
+
+        throw new InvalidArgumentException('Invalid amount');
     }
 }
